fd5af6ace5c5b6745cbd0ea9d3cda8f1347395b6
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-08-18 16:26:14 +0200

    Small Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import theo from "./images/dog1.png";
 function App() {
+  const [doggos, setDoggos] = useState(0);
+  const doggoClickHandler = () => {
+    setDoggos(prev => prev + 1);
+  }
   return (
     <div className="h-screen w-full flex flex-col font-lalezar text-center">
       <div className="h-[10vh] w-full flex text-4xl text-white bg-black justify-center items-center">
@@ -13,17 +18,23 @@ function App() {
             Your Doggo Farm
           </div>
           <div className="w-full bg-black bg-opacity-40 text-white py-3">
-            <div className=" text-2xl">0 Dogs</div>
+            <div className=" text-2xl">{doggos} Dogs</div>
             <div className=" text-xl">per second: 0.0 </div>
             </div>
             </div>
           <div className="flex flex-col gap-3 h-1/2">
             <div className="text-3xl text-white">Theo</div>
-            <img src={theo} alt="Theo" className="mr-8 h-full" />
+            <img src={theo} alt="Theo" className="mr-8 h-full" onClick={doggoClickHandler}/>
           </div>
           <div className="h-1/4 flex items-end pb-6">
-            <div className="text-6xl text-white">
-              <span className="-rotate-32 transform-gpu">C</span></div>
+            <div className="text-6xl text-white flex gap-1">
+              <div className="transform rotate-[20deg]">C</div>
+              <div className="transform rotate-[10deg] ">L</div>
+              <div className="">I</div>
+              <div className="transform -rotate-[10deg]">C</div>
+              <div className="transform -rotate-[20deg]">K</div>
+        
+            </div>
 
 
           </div>
@@ -31,7 +42,9 @@ function App() {
         </div>
         
         <div className="w-1/3 bg-[#90CCF4] border-r-8 border-black">bbbb</div>
-        <div className="w-1/3 bg-[#F78888]">cccc</div>
+        <div className="w-1/3 bg-[#F78888]">
+          
+        </div>
       </div>
     </div>
   );
