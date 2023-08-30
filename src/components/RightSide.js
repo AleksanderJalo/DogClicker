4d7f0381a91b03b6b6c5f9d88678400c753995be
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-08-30 16:58:56 +0200

    Cleanup/ Started working on upgrades

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import mouseBooth from "../images/upgrade art/mouse_booth.png";
 import goldenDogAnimation from "../images/upgrade art/Dog_Idle_Animation.gif";
 import brownWhiteDogAnimation from "../images/upgrade art/Dog_Idle_brown_white.gif";
 import dobermanDogWalkAnimation from "../images/upgrade art/Dog_2_Walk.gif";
+import Upgrade from "./Upgrade";
 const RightSide = () => {
+  const [goldenDogPrice, setGoldenDogPrice] = useState(15);
+
   return (
     <div className="w-1/3 bg-[#90CCF4] border-l-8 border-black flex flex-col  items-center">
       <div className="h-[10vh] w-full flex items-center justify-center">
@@ -20,61 +23,21 @@ const RightSide = () => {
         <img src={mouseBooth} alt="powerup" className="h-3/4" />
         <img src={mouseBooth} alt="powerup" className="h-3/4" />
       </div>
-      <div className="w-[90%] h-[8vh]  bg-[#D9D9D9] mt-6 border-8 border-[#988D94] flex items-center px-3 justify-between">
-        <img
-          src={goldenDogAnimation}
-          alt="doggo"
-          className="h-4/5 bg-[#988D94] border-2 border-black p-1"
-        />
-        <div>Golden Dog</div>
-        <div>0</div>
-      </div>
-      <div className="w-[90%] h-[8vh]  bg-[#D9D9D9] mt-6 border-8 border-[#988D94] flex items-center px-3 justify-between">
-        <img
-          src={dobermanDogWalkAnimation}
-          alt="doggo"
-          className="h-4/5 bg-[#988D94] border-2 border-black p-1"
-        />
-        <div>Doberman</div>
-        <div>0</div>
-      </div> 
-      <div className="w-[90%] h-[8vh]  bg-[#D9D9D9] mt-6 border-8 border-[#988D94] flex items-center px-3 justify-between">
-        <img
-          src={brownWhiteDogAnimation}
-          alt="doggo"
-          className="h-4/5 bg-[#988D94] border-2 border-black p-1"
-        />
-        <div>Brown Dog</div>
-        <div>0</div>
-      </div>
-
-      <div className="w-[90%] h-[8vh]  bg-[#D9D9D9] mt-6 border-8 border-[#988D94] flex items-center px-3 justify-between">
-        <img
-          src={goldenDogAnimation}
-          alt="doggo"
-          className="h-4/5 bg-[#988D94] border-2 border-black p-1"
-        />
-        <div>Golden Dog</div>
-        <div>0</div>
-      </div>
-      <div className="w-[90%] h-[8vh]  bg-[#D9D9D9] mt-6 border-8 border-[#988D94] flex items-center px-3 justify-between">
-        <img
-          src={goldenDogAnimation}
-          alt="doggo"
-          className="h-4/5 bg-[#988D94] border-2 border-black p-1"
-        />
-        <div>Golden Dog</div>
-        <div>0</div>
-      </div>
-      <div className="w-[90%] h-[8vh]  bg-[#D9D9D9] mt-6 border-8 border-[#988D94] flex items-center px-3 justify-between">
-        <img
-          src={goldenDogAnimation}
-          alt="doggo"
-          className="h-4/5 bg-[#988D94] border-2 border-black p-1"
-        />
-        <div>Golden Dog</div>
-        <div>0</div>
-      </div>
+      <Upgrade
+        image={goldenDogAnimation}
+        text={"Golden Dog"}
+        initialCost={15}
+      />
+      <Upgrade
+        image={dobermanDogWalkAnimation}
+        text={"Doberman"}
+        initialCost={100}
+      />
+      <Upgrade
+        image={brownWhiteDogAnimation}
+        text={"Brown Dog"}
+        initialCost={1100}
+      />
     </div>
   );
 };
