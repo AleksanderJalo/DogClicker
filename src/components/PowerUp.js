d0ba118594241622d65452ad46f7fe6c873bdf9e
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-09-10 11:13:18 +0200

    New PowerUpHandler/ switchin hover to another component

--- src/components/PowerUp.js
+++ src/components/PowerUp.js
@@ -1,38 +1,15 @@
-import React, { useEffect, useState } from "react";
-import dog from "../images/doggo.svg";
+import React from "react";
 const PowerUp = (props) => {
-  const [showInfo, setShowInfo] = useState(false);
-  const [price, setPrice] = useState(0);
-  useEffect(() => {
-    if (props.name === "Double Click") {
-      setPrice(100)
-    }
-  },[])
   return (
     <div
       className="relative h-[100%] flex justify-center items-center "
       onMouseEnter={() => {
-        setShowInfo(true);
+        props.onPowerUpHover(props.id);
       }}
       onMouseLeave={() => {
-        setShowInfo(false);
+        props.offPowerUpHover(props.id);
       }}
     >
-      {showInfo && (
-        <div className="absolute bottom-16 flex right-10  text-white bg-black border-[#955D09] border-8 px-3 pr-6">
-          <div className="flex flex-col">
-            <div className="flex justify-between items-center gap-8 pt-2 h-[50%]">
-              <div className="text-2xl whitespace-nowrap">{props.name}</div>
-              <div className="flex items-center justify-center gap-2  pb-1">
-                <img src={dog} className="h-[80%]" /> {price}
-              </div>
-            </div>
-            <div className="border-t-4 border-[#555454] flex justify-center items-center py-4 p-2 text-sm lg:text-lg h-[50%]">
-              {props.description}
-            </div>
-          </div>
-        </div>
-      )}
       <img className="h-[80%]" src={props.image} />
     </div>
   );
---
+++ src/components/PowerUpHandler.js
@@ -0,0 +1,18 @@
+export const getPowerUpInfo = (id) => {
+  switch (id) {
+    case 0:
+      return { name: "Double Click", description: "Your Clicks are doubled!" };
+    case 1:
+      return {
+        name: "Double Golden Dog",
+        description: "Your Golden Dogs make twice as much!",
+      };
+    case 2:
+      return {
+        name: "Double Doberman",
+        description: "Your Dobermans make twice as much!",
+          };
+          case 3:
+            return { name: "Double Brown Dogs", description: "Your Brown Dogs make twice as much!" };
+  }
+};
