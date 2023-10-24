e284b24da9aef402215a7ef91083e7ab763366f9
Aleksander Jałoszyński <aleksander.jaloszynski@gmail.com>
2023-10-24 18:48:03 +0200

    Responsive PowerUp layout

--- src/components/PowerUp.js
+++ src/components/PowerUp.js
@@ -3,14 +3,14 @@ const PowerUp = (props) => {
   return (
     <div
       onClick={props.onClick}
-      className="h-[100%] flex flex-col justify-center items-center gap-3"
+      className="h-[100%] flex flex-col justify-center items-center lg:gap-2"
     >
       <img
-        className={`h-[100%] ${props.show ? "opacity-100" : "opacity-50"}`}
+        className={`h-[35%] sm:h-[45%] md:h-[80%] lg:h-[100%] ${props.show ? "opacity-100" : "opacity-50"}`}
         src={props.image}
         alt="Power Up"
       />
-      <div className="text-white font-barlow text-[0.5rem] xl:text-[0.8rem]">{props.cost}</div>
+      <div className="text-white font-barlow text-[0.6rem] xl:text-[0.8rem] mb-[0.5rem]">{props.cost}</div>
     </div>
   );
 };
